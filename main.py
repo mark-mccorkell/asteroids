240ae1645a1eb9f12d95ced06441758e07485af3
mark-mccorkell <djmmccorkell@gmail.com>
2025-06-03 21:04:20 +0100

    Add constants.py and basic game loop

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
 
-    if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-        main()
+    # Main game loop
+    while True:
+        # Check for quit event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        # Blacken screen and refresh
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
