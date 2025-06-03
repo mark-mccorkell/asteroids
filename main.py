3aee670eed66d55da565e46742e4cb90bddd1535
mark-mccorkell <djmmccorkell@gmail.com>
2025-06-03 21:18:16 +0100

    Add FPS limit and delta time tracker

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Limit game speed
+    clock = pygame.time.Clock()
+    dt = 0
+
     # Main game loop
     while True:
         # Check for quit event
@@ -19,6 +23,9 @@ def main():
         # Blacken screen and refresh
         screen.fill("black")
         pygame.display.flip()
+
+        # Limit to 60fps and update delta time
+        dt  = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
