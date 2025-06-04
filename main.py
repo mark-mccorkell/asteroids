4493f133e6d5b160488178883b0e8452c8161bcd
mark-mccorkell <djmmccorkell@gmail.com>
2025-06-04 22:29:44 +0100

    Add sprite groups

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # Create groups to hold game objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+
     # Create a Player object in the centre of the screen
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -24,14 +31,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        # Update player position/angle
-        player.update(dt)
-        
+        # Update pall updatable objects
+        updatable.update(dt)
+        #player.update(dt)
+
         # Blacken screen
         screen.fill("black")
 
-        # Redraw the player
-        player.draw(screen)
+        # Redraw all redrawable objects
+        for obj in drawable:
+            obj.draw(screen)
+        #player.draw(screen)
         
 
         # Refresh the screen
